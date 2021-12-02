e3589a38effc4386cff56df8e6ca71e110542666
Tommy TIAN <xtianae@connect.ust.hk>
2021-12-02 13:34:10 +0800

    Day 2 - Dive

---
+++ 2_dive/2_dive.py
@@ -0,0 +1,45 @@
+class submarine:
+    def __init__(self):
+        self.horizontal_position = 0
+        self.vertical_position = 0
+        self.aim = 0
+    
+    def change_position(self, direction: str, distance: str) -> None:
+        if direction == "forward":
+            self.vertical_position += int(distance)
+        if direction == "up":
+            self.horizontal_position -= int(distance)
+        if direction == "down":
+            self.horizontal_position += int(distance)
+
+    def change_position_with_aim(self, direction: str, distance: str) -> None:
+        if direction == "forward":
+            self.vertical_position += int(distance)
+            self.horizontal_position += int(distance) * self.aim
+        if direction == "up":
+            self.aim -= int(distance)
+        if direction == "down":
+            self.aim += int(distance)
+
+    def get_position(self) -> str:
+        return f"{self.horizontal_position}, {self.vertical_position}"
+
+    def get_multiplication(self) -> int:
+        return self.horizontal_position * self.vertical_position
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        instructions = f.readlines()
+        s1 = submarine()
+        for instruction in instructions:
+            direction, distance = instruction.split()
+            s1.change_position(direction, distance)
+        print(s1.get_position())
+        print(s1.get_multiplication())
+
+        s2 = submarine()
+        for instruction in instructions:
+            direction, distance = instruction.split()
+            s2.change_position_with_aim(direction, distance)
+        print(s2.get_position())
+        print(s2.get_multiplication())
