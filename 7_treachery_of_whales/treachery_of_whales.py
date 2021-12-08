012d517b0f029a54b3d6f1978a789d3e5a7b817f
Tommy TIAN <xtianae@connect.ust.hk>
2021-12-08 10:27:07 +0800

    Day 7 - Treachery of Whales

--- 7_treachery_of_whales/treachery_of_whales.py
+++ 7_treachery_of_whales/treachery_of_whales.py
@@ -1,3 +1,4 @@
+import statistics
 from typing import List
 
 def get_inputs(file_name: str) -> List[int]:
@@ -8,27 +9,15 @@ def get_inputs(file_name: str) -> List[int]:
 def alignment_cost(crab_list: List[int], pivot: int) -> int:
     cost = 0
     for crab in crab_list:
-        cost += abs(crab - pivot)
-    return cost
-
-def alignment_cost_increased(crab_list: List[int], pivot: int) -> int:
-    cost = 0
-    for crab in crab_list:
         tmp = abs(crab - pivot)
         cost += tmp * (tmp + 1) // 2
     return cost
 
 def minimum_cost_alignment(crab_list: List[int]) -> int:
-    min_val = min(crab_list)
-    max_val = max(crab_list)
-    # cost is the maximum integer
-    cost = 2 ** 31 - 1
-    pivot = 0
-    for i in range(min_val, max_val + 1):
-        if alignment_cost(crab_list, i) < cost:
-            cost = alignment_cost(crab_list, i)
-            pivot = i
-    print(f'Pivot: {pivot}')
+    median = int(statistics.median(crab_list))
+    cost = 0
+    for crab in crab_list:
+        cost += abs(crab - median)
     return cost
 
 def minimum_cost_alignment_modified(crab_list: List[int]) -> int:
@@ -38,8 +27,8 @@ def minimum_cost_alignment_modified(crab_list: List[int]) -> int:
     cost = 2 ** 31 - 1
     pivot = 0
     for i in range(min_val, max_val + 1):
-        if alignment_cost_increased(crab_list, i) < cost:
-            cost = alignment_cost_increased(crab_list, i)
+        if alignment_cost(crab_list, i) < cost:
+            cost = alignment_cost(crab_list, i)
             pivot = i
     print(f'Pivot: {pivot}')
     return cost
