5416230215503a737b6ad1cd58f9c4964456aae6
Tommy TIAN <xtianae@connect.ust.hk>
2021-12-14 20:01:00 +0800

    Day 14 - Extended Polymerization

--- 14_extended_polymerization/extended_polymerization.py
+++ 14_extended_polymerization/extended_polymerization.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Dict
 
-def get_input(file_name:str) -> Tuple[str, Dict[str, str]]:
+
+def get_input(file_name: str) -> Tuple[str, Dict[str, str]]:
     with open(file_name, 'r') as f:
         data = f.readlines()
         original_polymer = data[0].strip()
@@ -10,8 +11,9 @@ def get_input(file_name:str) -> Tuple[str, Dict[str, str]]:
             rules[data[i][0]] = data[i][1]
         return original_polymer, rules
 
-def pair_insertion(polymer:str, rules:Dict[str, str], step: int) -> str:
-    for _ in range(step):
+
+def pair_insertion(polymer: str, rules: Dict[str, str], step: int) -> str:
+    for itr in range(step):
         new_polymer = ''
         for i in range(len(polymer) - 1):
             if polymer[i:i+2] in rules:
@@ -20,18 +22,21 @@ def pair_insertion(polymer:str, rules:Dict[str, str], step: int) -> str:
                 new_polymer += polymer[i]
         new_polymer += polymer[-1]
         polymer = new_polymer
+        print(itr, len(new_polymer), end='\n')
     return polymer
 
-def most_common_unit(polymer:str) -> int:
+
+def most_common_unit(polymer: str) -> int:
     units = {}
     for unit in polymer:
         if unit not in units:
             units[unit] = 0
         units[unit] += 1
-    most_common_unit =  max(units, key=units.get)
+    most_common_unit = max(units, key=units.get)
     return units[most_common_unit]
 
-def least_common_unit(polymer:str) -> int:
+
+def least_common_unit(polymer: str) -> int:
     units = {}
     for unit in polymer:
         if unit not in units:
@@ -40,7 +45,31 @@ def least_common_unit(polymer:str) -> int:
     least_common_unit = min(units, key=units.get)
     return units[least_common_unit]
 
-if __name__ == '__main__':
+
+def print_rules(rules: Dict[str, str]) -> None:
+    for key, value in rules.items():
+        print(key, value)
+
+
+def pair_insertion_optimized(polymer: str, rules: Dict[str, str], step: int) -> str:
+    for itr in range(step):
+        new_polymer = ''
+        for i in range(len(polymer) - 1):
+            if polymer[i:i+2] in rules:
+                new_polymer += (polymer[i] + rules[polymer[i:i+2]])
+            else:
+                new_polymer += polymer[i]
+        new_polymer += polymer[-1]
+        polymer = new_polymer
+        print(itr, len(new_polymer), end='\n')
+    return polymer
+
+
+def main() -> None:
     polymer, rules = get_input('input.txt')
     new_polymer = pair_insertion(polymer, rules, 40)
     print(most_common_unit(new_polymer) - least_common_unit(new_polymer))
+
+
+if __name__ == '__main__':
+    main()
