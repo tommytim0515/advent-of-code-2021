21f8bd352c76041d40672aadd6b4531335bf00ad
Tommy TIAN <xtianae@connect.ust.hk>
2021-12-22 13:07:05 +0800

    Day 18 Snailfish

---
+++ 18_snailfish/snailfish.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class SnailfishNumber:
+    def __init__(self, sequence: str):
+        pass
+
+def get_inputs(filename: str) -> List[str]:
+    with open(filename) as f:
+        data = f.readlines()
+        
+
+if __name__ == '__main__':
+    pass
